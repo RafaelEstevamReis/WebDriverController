aa129dfaaf9a12bb1865d54aef4128ab779b7552
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-12-17 17:45:58 -0300

    Update Program.cs

--- WebDriverController.Tests/Program.cs
+++ WebDriverController.Tests/Program.cs
@@ -12,36 +12,35 @@ var ctr = new WDController(driver);
 ctr.Navigated += navigated;
 
 ctr.Do(new Redirect("http://quotes.toscrape.com/"))
-    // wait until first quotes are visivle (author clicable)
+   // wait until first quotes are visible (author clickable)
    .WaitUntil_IsClickable(By.LinkText("(about)")) //.Do(new WaitUntil(By.LinkText("(about)"), WaitUntil.Is.Clickable))
    .Interact(By.LinkText("Login"), (w, e) => //.Do(new Interact(By.LinkText("Login"), (w, e) =>
    {
-           e.Click();
-    })
+       e.Click();
+   })
    .WaitUntil_IsClickable(By.XPath("//form/input[2]"))
    //.Do(new WaitUntil(By.XPath("//form/input[2]"), WaitUntil.Is.Clickable))
    .Inspect((WDController c, IWebDriver d) =>
-       {
-           d.FindElement(By.Id("username"))
-            .SendKeys("me@myself.com");
-           d.FindElement(By.Id("password"))
-           .SendKeys("secret");
+   {
+       d.FindElement(By.Id("username"))
+        .SendKeys("me@myself.com");
+       d.FindElement(By.Id("password"))
+       .SendKeys("secret");
 
-           // take proof
-           var ss = c.GetScreenshot();
+       // take proof
+       var ss = c.GetScreenshot();
 
-           d.FindElement(By.XPath("//form/input[2]"))
-            .Submit();
-       })
+       d.FindElement(By.XPath("//form/input[2]"))
+        .Submit();
+   })
    .Inspect((WDController c, IWebDriver d) =>
-       {
-           Console.WriteLine("Logged in !");
-           // all html is at:
-           var html = c.PageSource;
-           // Extract all the things !
-           var document = HtmlParseHelper.ParseHtmlDocument(html);
-
-       })
+   {
+       Console.WriteLine("Logged in !");
+       // all html is at:
+       var html = c.PageSource;
+       // Extract all the things !
+       var document = HtmlParseHelper.ParseHtmlDocument(html);
+   })
    // end the party
    .Quit();
 
