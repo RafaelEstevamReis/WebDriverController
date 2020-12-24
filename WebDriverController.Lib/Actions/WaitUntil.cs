e310300f95dca8ac77f5ee6c3d6d809c2a060260
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-12-23 21:43:09 -0300

    Add WaitUntil_IsDestroyed and WaitUntil_UserNavigate

--- WebDriverController.Lib/Actions/WaitUntil.cs
+++ WebDriverController.Lib/Actions/WaitUntil.cs
@@ -12,10 +12,11 @@ namespace RafaelEstevam.WebDriverController.Lib.Actions
             Exists,
             Visible,
             Clickable,
+            Destroyed,
         }
 
         public IWDActionResult Result { get; set; } = new Results.None();
-        public TimeSpan Timeout { get; set; } = TimeSpan.FromSeconds(10);
+        public TimeSpan Timeout { get; set; } = TimeSpan.FromMinutes(1);
         public By Element { get; }
         public Is Test { get; }
 
@@ -29,45 +30,72 @@ namespace RafaelEstevam.WebDriverController.Lib.Actions
         {
             var wait = new WebDriverWait(driver, Timeout);
 
-            wait.Until((driver) =>
+            if (Test == Is.Destroyed)
             {
-                IWebElement wElement;
+                // has to exists here
+                IWebElement wElement = driver.FindElement(Element);
 
-                try
+                wait.Until((driver) =>
                 {
-                    wElement = driver.FindElement(Element);
-                }
-                catch (NoSuchElementException)
-                {
-                    wElement = null;
-                }
+                    try
+                    {
+                        // des not matter, should check something
+                        bool visible = wElement.Enabled;
+                        if (visible)
+                        {
+                            // not staled
+                            return false;
+                        }
+                    }
+                    catch (StaleElementReferenceException){ return true; }
 
-                if (Test == Is.Exists)
-                {
-                    return wElement != null;
-                }
-
-                if (wElement == null) return false;
-                
-                if (Test == Is.Visible)
-                {
-                    return wElement.Displayed;
-                }
-                else if (Test == Is.Clickable)
-                {
-                    return wElement.Displayed && wElement.Enabled;
-                }
-
-                return false;
-            });
+                    return false;
+                });
+            }
+            else
+            {
+                wait.Until(checkNonDestroyed);
+            }
 
             return Result;
         }
 
+        private bool checkNonDestroyed(IWebDriver driver)
+        {
+            IWebElement wElement;
+
+            try
+            {
+                wElement = driver.FindElement(Element);
+            }
+            catch (NoSuchElementException)
+            {
+                wElement = null;
+            }
+
+            if (Test == Is.Exists)
+            {
+                return wElement != null;
+            }
+
+            if (wElement == null) return false;
+
+            if (Test == Is.Visible)
+            {
+                return wElement.Displayed;
+            }
+            else if (Test == Is.Clickable)
+            {
+                return wElement.Displayed && wElement.Enabled;
+            }
+
+            return false;
+        }
 
         public static WaitUntil Exists(By Element) => new WaitUntil(Element, Is.Exists);
         public static WaitUntil IsClickable(By Element) => new WaitUntil(Element, Is.Clickable);
         public static WaitUntil IsVisible(By Element) => new WaitUntil(Element, Is.Visible);
+        public static WaitUntil IsDestroyed(By Element) => new WaitUntil(Element, Is.Destroyed);
     }
     public static class InteractWaitUntil
     {
@@ -75,13 +103,52 @@ namespace RafaelEstevam.WebDriverController.Lib.Actions
         {
             return controller.Do(WaitUntil.IsVisible(locator));
         }
+        public static WDController WaitUntil_Visible(this WDController controller, By locator, TimeSpan timeout)
+        {
+            var w = WaitUntil.IsVisible(locator);
+            w.Timeout = timeout;
+            return controller.Do(w);
+        }
         public static WDController WaitUntil_IsClickable(this WDController controller, By locator)
         {
             return controller.Do(WaitUntil.IsClickable(locator));
         }
+        public static WDController WaitUntil_IsClickable(this WDController controller, By locator, TimeSpan timeout)
+        {
+            var w = WaitUntil.IsClickable(locator);
+            w.Timeout = timeout;
+            return controller.Do(w);
+        }
         public static WDController WaitUntil_Exists(this WDController controller, By locator)
         {
             return controller.Do(WaitUntil.Exists(locator));
+        }
+        public static WDController WaitUntil_Exists(this WDController controller, By locator, TimeSpan timeout)
+        {
+            var w = WaitUntil.Exists(locator);
+            w.Timeout = timeout;
+            return controller.Do(w);
+        }
+        public static WDController WaitUntil_IsDestroyed(this WDController controller, By locator)
+        {
+            return controller.Do(WaitUntil.IsDestroyed(locator));
+        }
+        public static WDController WaitUntil_IsDestroyed(this WDController controller, By locator, TimeSpan timeout)
+        {
+            var w = WaitUntil.IsDestroyed(locator);
+            w.Timeout = timeout;
+            return controller.Do(w);
+        }
+
+        public static WDController WaitUntil_UserNavigate(this WDController controller)
+        {
+            return WaitUntil_UserNavigate(controller, TimeSpan.FromMinutes(2));
+        }
+        public static WDController WaitUntil_UserNavigate(this WDController controller, TimeSpan timeout)
+        {
+            var w = WaitUntil.IsDestroyed(By.TagName("body"));
+            w.Timeout = timeout;
+            return controller.Do(w);
         }
     }
 }
