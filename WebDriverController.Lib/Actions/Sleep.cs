6c2883b1c62241fd5c430f44619b4f2400f899b1
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-12-23 21:42:40 -0300

    Add Sleep action

---
+++ WebDriverController.Lib/Actions/Sleep.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using RafaelEstevam.WebDriverController.Lib.Interfaces;
+using RafaelEstevam.WebDriverController.Lib.Results;
+
+namespace RafaelEstevam.WebDriverController.Lib.Actions
+{
+    public class Sleep : IWDAction
+    {
+        private readonly TimeSpan span;
+
+        public Sleep(TimeSpan span)
+        {
+            this.span = span;
+        }
+
+        public IWDActionResult Execute(WDController wDController, IWebDriver driver)
+        {
+            Task.Delay(span).Wait();
+            return new None();
+        }
+    }
+    public static class SleepExtension
+    {
+        public static WDController Sleep(this WDController controller, TimeSpan span)
+        {
+            return controller.Do(new Sleep(span));
+        }
+        public static WDController Sleep(this WDController controller, int timeInMilliseconds)
+        {
+            return controller.Do(new Sleep(TimeSpan.FromMilliseconds(timeInMilliseconds)));
+        }
+    }
+}
