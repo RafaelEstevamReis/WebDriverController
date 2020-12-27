fe20b8c02ef6990971f0c15c18906ef4d6405e76
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-12-27 00:15:00 -0300

    Add HtmlAgilityPack support

---
+++ WebDriverController.Lib/Extensions/WebElementExtensions.cs
@@ -0,0 +1,24 @@
+﻿using HtmlAgilityPack;
+using OpenQA.Selenium;
+
+namespace RafaelEstevam.WebDriverController.Lib.Extensions
+{
+    public static class WebElementExtensions
+    {
+        public static string GetInnerHtml(this IWebElement element)
+        {
+            // https://quirksmode.org/dom/html/
+            return element.GetAttribute("innerHTML");
+        }
+        public static string GetOuterHTML(this IWebElement element)
+        {
+            // https://quirksmode.org/dom/html/
+            return element.GetAttribute("outerHTML");
+        }
+        public static HtmlNode GetHtmlDocument(this IWebElement element)
+        {
+            return HtmlNode.CreateNode(element.GetOuterHTML());
+        }       
+
+    }
+}
