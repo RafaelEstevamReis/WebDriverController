8ca418bf013d3240cd8974c40b9cd662d812bdaf
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-12-26 23:10:38 -0300

    Add scroll options

---
+++ WebDriverController.Lib/Actions/Scroll.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+
+namespace RafaelEstevam.WebDriverController.Lib.Actions
+{
+    public static class ScrollExtensions
+    {
+        public static WDController ScrollTo(this WDController controller, IWebElement element)
+        {
+            controller.ExecuteScript("arguments[0].scrollIntoView(true);", element);
+            return controller;
+        }
+        public static WDController ScrollToCenter(this WDController controller, IWebElement element)
+        {
+            controller.ExecuteScript("arguments[0].scrollIntoView({block: 'center'});", element);
+            return controller;
+        }
+        public static WDController ScrollTo(this WDController controller, int XPos, int YPos)
+        {
+            controller.ExecuteScript($"window.scrollTo({XPos}, {YPos});");
+            return controller;
+        }
+        public static WDController ScrollTop(this WDController controller)
+        {
+            controller.ExecuteScript("document.documentElement.scrollTop = 0;");
+            return controller;
+        }
+        public static WDController ScrollBottom(this WDController controller)
+        {
+            controller.ExecuteScript("window.scrollTo(0, document.body.scrollHeight);");
+            return controller;
+        }
+    }
+}
