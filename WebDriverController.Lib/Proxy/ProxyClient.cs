36abf43a8c51e0ae66cdc0664467735b8c5681ed
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2020-12-30 17:53:16 -0300

    Cleanup

--- WebDriverController.Lib/Proxy/ProxyClient.cs
+++ WebDriverController.Lib/Proxy/ProxyClient.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net;
-using System.Net.Sockets;
-using System.Text;
+﻿using System.Net.Sockets;
 using System.Threading.Tasks;
 
 namespace RafaelEstevam.WebDriverController.Proxy
