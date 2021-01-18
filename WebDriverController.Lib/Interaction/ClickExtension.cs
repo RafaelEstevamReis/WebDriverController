863b84915fb0feca17360eba5d3a26698deb7bce
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2021-01-18 18:20:44 -0300

    Add interaction

---
+++ WebDriverController.Lib/Interaction/ClickExtension.cs
@@ -0,0 +1,13 @@
+﻿using OpenQA.Selenium;
+
+namespace RafaelEstevam.WebDriverController.Interaction
+{
+    public static class ClickExtension
+    {
+        public static Controller ClickOn(this Controller controller, By locator)
+        {
+            controller.FindElement(locator).Click();
+            return controller;
+        }
+    }
+}
---
+++ WebDriverController.Lib/Interaction/HoverExtensions.cs
@@ -0,0 +1,18 @@
+﻿using OpenQA.Selenium;
+
+namespace RafaelEstevam.WebDriverController.Interaction
+{
+    public static class HoverExtensions
+    {
+        public static Controller HoverOn(this Controller controller, By locator)
+        {
+            var action = new OpenQA.Selenium.Interactions.Actions(controller.WrappedDriver);
+            var we = controller.FindElement(locator);
+            action.MoveToElement(we)
+                  .Build()
+                  .Perform();
+
+            return controller;
+        }
+    }
+}
